942ba0a0907fa2a6d43cba47ff4853a542e15cda
Long Long <dragon9968@gmail.com>
2026-01-20 15:09:35 +0700

    Add checkout flow automation tests

---
+++ tests/pageUI/ProductPageUI.ts
@@ -0,0 +1,8 @@
+export const ProductPageUI = {
+  ADD_TO_CART_BUTTON: "//button[@title='Add to Cart']",
+  CART_ICON: ".skip-cart",
+  VIEW_CART: "a.viewcart",
+   // popup warning (HTML)
+  WARNING_POPUP: "div.ui-dialog", 
+  WARNING_ACCEPT_BUTTON: "button:has-text('Accept')",
+};
